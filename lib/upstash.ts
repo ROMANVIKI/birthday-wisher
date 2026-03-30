59314f90882702ef32d5e70d1510f7ba4ea89eca
ROMANVIKI <vikiadhi2016@gmail.com>
2026-03-30 10:28:40 +0530

    updated upstash envs

--- lib/upstash.ts
+++ lib/upstash.ts
@@ -1,9 +1,10 @@
 import { Redis } from '@upstash/redis'
 import type { Wish } from '@/types'
 
+
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  url: process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN!,
 })
 
 const TTL = 60 * 60 * 24 * 30 // 30 days
